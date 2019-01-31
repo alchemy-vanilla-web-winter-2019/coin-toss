59acba9047a977c4ae82ddad57358ce58ff0566f
Alexandre Muniz <aom@puzzlezapper.com>
2019-01-30 21:18:02 -0800

    added images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import headsTails from './heads-tails.js';
 const tossForm = document.getElementById('toss-form');
+const coinImage = document.getElementById('coin-image');
 
 tossForm.addEventListener('submit', function(event) {
     event.preventDefault();
     console.log('form submitted');  
     const randomNumber = Math.random;
-    const face = headsTails(randomNumber);
+    const face = headsTails(randomNumber());
     console.log(face);
+    coinImage.src = "../assets/" + face + ".jpg";
+    coinImage.classList.remove('hidden');
 });

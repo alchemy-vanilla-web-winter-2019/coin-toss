42356d0e83bf540a062f74b48c81d1ff15bac16a
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-01-30 18:47:47 -0700

    added second test to meet condition for tails

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,9 @@ function headsOrTails(randomNumber) {
     if(randomNumber < 0.5) {
         return 'heads';
     }
+    else {
+        return 'tails';
+    }
 }
 
 
@@ -12,5 +15,11 @@ function headsOrTails(randomNumber) {
 test('if number is less than 0.5, return heads', function(assert) {
     const results = headsOrTails(0.3);
     assert.equal(results, 'heads');
+
+});
+
+test('if number is greater than or equal to 0.5, return tails', function(assert) {
+    const results = headsOrTails(0.6);
+    assert.equal(results, 'tails');
 
 });

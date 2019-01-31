c1723021f679b846882259be9677e9b6ecec75a7
bpkotowski <bighoopee@gmail.com>
2019-01-30 17:58:38 -0800

    defined structure

---
+++ test/tests.js
@@ -0,0 +1,10 @@
+const test = QUnit.test;
+
+
+const images = document.getElementById(images);
+console.log(images);
+
+test('get image nodes', function(assert) {
+
+    assert.equal(true, false);
+});

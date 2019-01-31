2c5a21643abe4c93cba44ce1ab7e0c1f5a56a5b3
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-30 19:12:52 -0800

    added counts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import headsOrTails from './heads-or-tails.js';
 
 const flipForm = document.getElementById('flip-form');
 const faceImage = document.getElementById('face-image');
+const headsCount = document.getElementById('heads-count');
+const tailsCount = document.getElementById('tails-count');
 
+let heads = 0;
+let tails = 0;
 flipForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -12,9 +16,13 @@ flipForm.addEventListener('submit', function(event) {
 
     if(face === 'heads') {
         faceImageSrc = '../assets/heads.jpg';
+        heads++;
     } else {
         faceImageSrc = '../assets/tails.jpg';
+        tails++;
     }
     faceImage.src = faceImageSrc;
     faceImage.classList.remove('hidden');
+    headsCount.textContent = heads;
+    tailsCount.textContent = tails;
 });

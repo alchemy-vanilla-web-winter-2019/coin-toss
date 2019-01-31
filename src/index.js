3d7a63b2fbf8573dca60c3a94ed81aa9811f9855
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-01-30 19:05:12 -0700

    finished goals, have heads and tails count counting and rendering

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import headsOrTails from '../test/heads-tails.js';
-const image = document.getElementById('coin-image');
-
 const button = document.getElementById('coin-button');
+const image = document.getElementById('coin-image');
+const headCount = document.getElementById('head-count');
+const tailCount = document.getElementById('tail-count');
+let head = 0;
+let tail = 0;
 
 button.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -9,9 +12,14 @@ button.addEventListener('submit', function(event) {
     let side = headsOrTails(randomNumber);
     if(side === 'heads') {
         image.src = './assets/heads.jpg';
+        head++;
+        headCount.textContent = 'Head Count: ' + head;
+
     }
     else {
         image.src = './assets/tails.jpg';
+        tail++;
+        tailCount.textContent = 'Tail Count: ' + tail;
 
     }
 

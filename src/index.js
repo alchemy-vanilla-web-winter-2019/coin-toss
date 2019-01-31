0710388c73dd0477068e461093d933d64d71398d
Leslie Shea <leslieyshea@gmail.com>
2019-01-30 20:32:24 -0800

    cleaned up

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const image = document.getElementById('coin-pic');
 const headsNode = document.getElementById('heads-count');
 const tailsNode = document.getElementById('tails-count');
 
-
 let headsCount = 0;
 let tailsCount = 0;
 

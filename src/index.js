d30ed250b0573a8195f7fcd362b5b7e2cf5aef8c
Cara Ponzini <ponzini@gmail.com>
2019-01-30 18:02:19 -0800

    a wooden coin toss with counter

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import drinkOrDisney from './drink-or-disney.js';
 const coinForm = document.getElementById('coin-form');
 const image = document.getElementById('coin-image');
 const result = document.getElementById('result');
+const drinkCount = document.getElementById('drink-count');
+const disneyCount = document.getElementById('disney-count');
 
-
+let drinks=0;
+let disney=0;
 
 coinForm.addEventListener('submit', function(event){
     event.preventDefault();
@@ -20,10 +23,14 @@ coinForm.addEventListener('submit', function(event){
     if(side === 'drink'){
         image.src = './assets/drink.jpeg';
         result.textContent = 'Yay! Free Drink!';
+        drinks++;
+        drinkCount.textContent = 'Free Drinks: ' + drinks;  
     }
     else {
         image.src = './assets/disney.jpeg';
         result.textContent = 'Yay! Disneyland!';
+        disney++;
+        disneyCount.textContent = 'Trips to Disney: ' + disney;  
     }
 
 });

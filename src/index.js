5712a66f6112f35bc1adf887d7fe019705538091
Cheri Portman <cheriportman@hotmail.com>
2019-01-30 20:13:13 -0800

    can console log my toss Result, working on attaching to selected image

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import flipIt from './flip-it.js';
 const flipForm = document.getElementById('flip-form');
-
+const flipImage = document.getElementById('flip-image');
 
 
 flipForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const randomNumber = Math.random();
-    console.log(randomNumber);
 
+    const tossResult = flipIt(randomNumber);
+    //above I have the heads or tails results from flipIt
+    console.log(tossResult);
+    // let imageSource = '';
+    
 });
 

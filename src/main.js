53c55dd464fbe86a893132bec291801cb57ad4a6
Ben Parisot <me@benparisot.com>
2019-01-30 17:40:22 -0800

    get image switch to work with submit event

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 import coinFlip from './coin-flip.js';
 
-const 
+const picNode = document.getElementById('coin-pic');
+const flipForm = document.getElementById('flip-form');
+
+
+flipForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const randomNumber = Math.random();
+    const result = coinFlip(randomNumber);
+    let imageSource = '';
+
+    if(result === 'heads') {
+        imageSource = '../assets/coin-horse.jpg';
+    }
+    else {
+        imageSource = '../assets/coin-button.jpg';
+
+    }
+    picNode.src = imageSource;
+    console.log(result);
+    
+
+});

0be0523e3a7e514b76f21914484e62dbc92d3f2b
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-30 20:39:36 -0800

    added status

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const flipForm = document.getElementById('flip-form');
 const faceImage = document.getElementById('face-image');
 const headsCount = document.getElementById('heads-count');
 const tailsCount = document.getElementById('tails-count');
+const counts = document.getElementById('counts');
+const status = document.getElementById('status');
 
 let heads = 0;
 let tails = 0;
@@ -17,12 +19,16 @@ flipForm.addEventListener('submit', function(event) {
     if(face === 'heads') {
         faceImageSrc = '../assets/heads.jpg';
         heads++;
+        status.textContent = 'Heads!';
     } else {
         faceImageSrc = '../assets/tails.jpg';
         tails++;
+        status.textContent = 'Tails!';
     }
     faceImage.src = faceImageSrc;
     faceImage.classList.remove('hidden');
+
     headsCount.textContent = heads;
     tailsCount.textContent = tails;
+    counts.classList.remove('hidden');
 });

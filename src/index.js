8cee90572a3cb872c2d3ddb78bd30ab955b17c4e
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-01-30 18:57:03 -0700

    getting both sides of coin to render randomly in DOM when button is clicked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-//import headsOrTails 
+import headsOrTails from '../test/heads-tails.js';
+const image = document.getElementById('coin-image');
 
 const button = document.getElementById('coin-button');
 
@@ -6,9 +7,16 @@ button.addEventListener('submit', function(event) {
     event.preventDefault();
     const randomNumber = Math.random();
     let side = headsOrTails(randomNumber);
+    if(side === 'heads') {
+        image.src = './assets/heads.jpg';
+    }
+    else {
+        image.src = './assets/tails.jpg';
+
+    }
 
 
 
-
+    image.classList.remove('hidden');
 
 });

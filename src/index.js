2f8130bb41e9c7a6a618eed3e785d19ea2dd6f6a
emily baier <emilyrbaier@gmail.com>
2019-01-30 22:26:38 -0800

    added styling and finished flipping code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const coinForm = document.getElementById('toss-coin-button');
 const image = document.getElementById('coin-image');
 const result = document.getElementById('results');
 
+
 coinForm.addEventListener('submit', function(event) {
     event.preventDefault();
     //console.log('button pressed');
@@ -13,5 +14,18 @@ coinForm.addEventListener('submit', function(event) {
 
     const coin = coinToss(randomNumber);
 
+    if(coin === 'heads') {
+        image.src = './assets/heads.jpg';
+        result.textContent = 'It\'s Heads!';
+        console.log('heads');
+    }
+
+    else {
+        image.src = './assets/tails.png';
+        result.textContent = 'It\'s Tails!';
+        console.log('tails');
+    }
+
+    image.classList.remove ('hidden');
 
 });

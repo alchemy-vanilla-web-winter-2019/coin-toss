f5b21b99e32392799fb1a7a179df6b0893967ca0
Parker Helland <hellandp@gmail.com>
2019-02-03 22:32:24 -0800

    added images and linked to click me button

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import coinToss from './coin-toss.js';
 
 const pickForm = document.getElementById('pick-form');
+const image = document.getElementById('coin-image');
 
 pickForm.addEventListener('submit', function(event){
     event.preventDefault();
     const randomNumber = Math.random();
-    const face = coinToss(randomNumber);
-    
-    
+    let face = coinToss(randomNumber);
+    if(face === 'Heads') {
+        image.src = '/assets/tails.jpeg';
+    } else {
+        image.src = 'assets/heads.jpeg';
+    }
+
     console.log(face);
     console.log(randomNumber);
-
+    image.classList.remove('hidden');
 
 });

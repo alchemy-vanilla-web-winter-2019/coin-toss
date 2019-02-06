070cbb11bf064291fc2de3bba0ea49e879d04e21
Cheri Portman <cheriportman@hotmail.com>
2019-02-05 16:09:28 -0800

    working on user-predict feature

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import flipIt from './flip-it.js';
 const flipForm = document.getElementById('flip-form');
 const flipImage = document.getElementById('flip-image');
+const flipPredict = document.getElementsByName('predict');
 
-
+console.log('flippredict', flipPredict);
 flipForm.addEventListener('submit', function(event) {
     event.preventDefault();
+    // here I need to get the value from the radio buttons
+    //check using if/else which is checked, then carry on
+    const userPick = flipPredict;
 
+    console.log('userPick', userPick);
+    
+    
     const randomNumber = Math.random();
 
     const tossResult = flipIt(randomNumber);
